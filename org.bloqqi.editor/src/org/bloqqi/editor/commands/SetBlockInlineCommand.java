bloqqi/bloqqi-editor

commit 10be9a61ddd90aa796f14f07293814ac79284b65

Compiler AST changed (Component -> Block)

org.bloqqi.editor/src/org/bloqqi/editor/commands/SetBlockInlineCommand.java
@@ -6,29 +6,29 @@ import java.util.Map;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.commands.Command;
 import org.bloqqi.compiler.ast.ASTNode;
-import org.bloqqi.compiler.ast.Component;
+import org.bloqqi.compiler.ast.Block;
 import org.bloqqi.compiler.ast.DiagramType;
-import org.bloqqi.compiler.ast.InheritedComponent;
+import org.bloqqi.compiler.ast.InheritedBlock;
 import org.bloqqi.compiler.ast.Node;
 import org.bloqqi.editor.Coordinates;
 import org.bloqqi.editor.autolayout.AutoLayoutKlay;
 
-public class SetComponentInlineCommand extends Command {
+public class SetBlockInlineCommand extends Command {
 	private final static int RELATIVE_X_OFFSET = -50;
 	private final static int RELATIVE_Y_OFFSET = -30;
 	
 	private final Coordinates coordinates;
 	private final DiagramType diagramType;
-	private final Component component;
+	private final Block block;
 	private final boolean newInlineValue;
 	private final boolean oldInlineValue;
 	
-	public SetComponentInlineCommand(Coordinates coordinates, Component component, boolean newInlineValue) {
+	public SetBlockInlineCommand(Coordinates coordinates, Block block, boolean newInlineValue) {
 		this.coordinates = coordinates;
-		this.diagramType = component.diagramType();
-		this.component = component;
+		this.diagramType = block.diagramType();
+		this.block = block;
 		this.newInlineValue = newInlineValue;
-		this.oldInlineValue = component.getModifiers().isInline();
+		this.oldInlineValue = block.getModifiers().isInline();
 	}
 	
 	@Override
@@ -42,20 +42,20 @@ public class SetComponentInlineCommand extends Command {
 			// Compute the coordinate before the change
 			autoLayoutCollapsedBlock();
 		}
-		component.getModifiers().setModifier("inline", newInlineValue);
-		component.program().flushAllAttributes();
+		block.getModifiers().setModifier("inline", newInlineValue);
+		block.program().flushAllAttributes();
 		if (newInlineValue) {
 			// Compute the coordinates after the change
 			autoLayoutInlinedBlocks();
 		}
-		component.diagramType().notifyObservers();
+		block.diagramType().notifyObservers();
 	}
 
 	private void autoLayoutInlinedBlocks() {
-		// Compute auto layout for the inlined components and adjust them
-		// according to the position of the component that is inlined.
-		Rectangle componentRectangle = coordinates.getRectangle(diagramType, component.accessString());
-		DiagramType dt = (DiagramType) component.type();
+		// Compute auto layout for the inlined blocks and adjust them
+		// according to the position of the blocks that is inlined.
+		Rectangle blockRectangle = coordinates.getRectangle(diagramType, block.accessString());
+		DiagramType dt = (DiagramType) block.type();
 		Map<Node, Rectangle> layout = new AutoLayoutKlay(dt, false).layoutAsNodeMap();
 		Map<String, Rectangle> adjustedLayout = new HashMap<>();
 		for (Map.Entry<Node, Rectangle> e: layout.entrySet()) {
@@ -67,42 +67,42 @@ public class SetComponentInlineCommand extends Command {
 			if (name.contains(ASTNode.DECLARED_IN_SEP)) {
 				name = name.substring(name.indexOf(ASTNode.DECLARED_IN_SEP)+ASTNode.DECLARED_IN_SEP.length());
 			}
-			String key = component.accessString() + ASTNode.INLINE_SEP + name;
+			String key = block.accessString() + ASTNode.INLINE_SEP + name;
 			
-			r.x += componentRectangle.x + RELATIVE_X_OFFSET;
-			r.y += componentRectangle.y + RELATIVE_Y_OFFSET;
+			r.x += blockRectangle.x + RELATIVE_X_OFFSET;
+			r.y += blockRectangle.y + RELATIVE_Y_OFFSET;
 			
 			adjustedLayout.put(key, r);
 		}
 
-		// Update coordinates of the inlined components
-		for (InheritedComponent c: diagramType.getComponents()) {
-			if (c.isInlined()
-					&& c.inlinedComponent() == component
-					&& coordinates.getRectangle(diagramType, c.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
-				Rectangle r = adjustedLayout.get(c.accessString());
+		// Update coordinates of the inlined blocks
+		for (InheritedBlock block: diagramType.getBlocks()) {
+			if (block.isInlined()
+					&& block.inlinedBlock() == block
+					&& coordinates.getRectangle(diagramType, block.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
+				Rectangle r = adjustedLayout.get(block.accessString());
 				if (r == null) r = Coordinates.RECTANGLE_NOT_FOUND;
 				coordinates.setRectangle(
 						diagramType,
-						c.accessString(),
+						block.accessString(),
 						r);
 			}
 		}
 	}
 
 	private void autoLayoutCollapsedBlock() {
-		if (coordinates.getRectangle(diagramType, component.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
+		if (coordinates.getRectangle(diagramType, block.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
 			Rectangle closestToOrigin = null;
-			for (InheritedComponent c: diagramType.getComponents()) {
-				if (c.isInlined() && c.inlinedComponent() == component) {
-					Rectangle r = coordinates.getRectangle(diagramType, c.accessString());
+			for (InheritedBlock block: diagramType.getBlocks()) {
+				if (block.isInlined() && block.inlinedBlock() == block) {
+					Rectangle r = coordinates.getRectangle(diagramType, block.accessString());
 					if (closestToOrigin == null || distance(r) < distance(closestToOrigin)) {
 						closestToOrigin = r;
 					}
 				}
 			}
 			if (closestToOrigin != null) {
-				coordinates.setRectangle(diagramType, component.accessString(), closestToOrigin);
+				coordinates.setRectangle(diagramType, block.accessString(), closestToOrigin);
 			}
 		}
 	}
@@ -118,8 +118,8 @@ public class SetComponentInlineCommand extends Command {
 
 	@Override
 	public void undo() {
-		component.getModifiers().setModifier("inline", oldInlineValue);
-		component.program().flushAllAttributes();
-		component.diagramType().notifyObservers();
+		block.getModifiers().setModifier("inline", oldInlineValue);
+		block.program().flushAllAttributes();
+		block.diagramType().notifyObservers();
 	}
 }

org.bloqqi.editor/src/org/bloqqi/editor/Actions.java
@@ -6,11 +6,11 @@ import org.bloqqi.editor.actions.AutoLayoutAction;
 import org.bloqqi.editor.actions.ChangeAllConnectionRoutersAction;
 import org.bloqqi.editor.actions.ChangeConnectionRouterAction;
 import org.bloqqi.editor.actions.ChangeNameDisplayedAction;
-import org.bloqqi.editor.actions.ChangeSpecializationComponent;
+import org.bloqqi.editor.actions.ChangeSpecializationBlock;
 import org.bloqqi.editor.actions.DiagramTypePropertiesAction;
 import org.bloqqi.editor.actions.ExtractSubTypeAsRecommendationAction;
 import org.bloqqi.editor.actions.InlineAction;
-import org.bloqqi.editor.actions.OpenComponentTypeAction;
+import org.bloqqi.editor.actions.OpenBlockTypeAction;
 import org.bloqqi.editor.actions.RenameAction;
 import org.bloqqi.editor.actions.SourceInterceptAction;
 import org.bloqqi.editor.actions.TargetInterceptAction;
@@ -69,8 +69,8 @@ public class Actions {
 		addSelectionAction(new InlineAction(editor));
 		addSelectionAction(new TargetInterceptAction(editor));
 		addSelectionAction(new SourceInterceptAction(editor));
-		addSelectionAction(new OpenComponentTypeAction(editor));
-		addSelectionAction(new ChangeSpecializationComponent(editor));
+		addSelectionAction(new OpenBlockTypeAction(editor));
+		addSelectionAction(new ChangeSpecializationBlock(editor));
 	}
 	
 	private void createOutlineActions() {

org.bloqqi.editor/src/org/bloqqi/editor/ContextMenu.java
@@ -3,11 +3,11 @@ package org.bloqqi.editor;
 import org.bloqqi.editor.actions.AutoLayoutAction;
 import org.bloqqi.editor.actions.ChangeAllConnectionRoutersAction;
 import org.bloqqi.editor.actions.ChangeConnectionRouterAction;
-import org.bloqqi.editor.actions.ChangeSpecializationComponent;
+import org.bloqqi.editor.actions.ChangeSpecializationBlock;
 import org.bloqqi.editor.actions.DiagramTypePropertiesAction;
 import org.bloqqi.editor.actions.ExtractSubTypeAsRecommendationAction;
 import org.bloqqi.editor.actions.InlineAction;
-import org.bloqqi.editor.actions.OpenComponentTypeAction;
+import org.bloqqi.editor.actions.OpenBlockTypeAction;
 import org.bloqqi.editor.actions.RenameAction;
 import org.bloqqi.editor.actions.SourceInterceptAction;
 import org.bloqqi.editor.actions.TargetInterceptAction;
@@ -51,10 +51,10 @@ public class ContextMenu extends ContextMenuProvider {
 			addAction(layoutSubmenu, ChangeConnectionRouterAction.ID);
 		}
 		
-		addActionToRest(menu, OpenComponentTypeAction.ID);
+		addActionToRest(menu, OpenBlockTypeAction.ID);
 		addActionToRest(menu, InlineAction.ID);
 		addActionToRest(menu, RenameAction.ID);
-		addActionToRest(menu, ChangeSpecializationComponent.ID);
+		addActionToRest(menu, ChangeSpecializationBlock.ID);
 		addActionToRest(menu, ActionFactory.DELETE.getId());
 		
 		menu.appendToGroup(GEFActionConstants.GROUP_EDIT, new Separator());

org.bloqqi.editor/src/org/bloqqi/editor/Coordinates.java
@@ -11,11 +11,11 @@ import org.eclipse.draw2d.geometry.Rectangle;
 import org.bloqqi.compiler.ast.ASTNode;
 import org.bloqqi.compiler.ast.DiagramType;
 import org.bloqqi.compiler.ast.Node;
-import org.bloqqi.editor.figures.ComponentFigure;
+import org.bloqqi.editor.figures.BlockFigure;
 
 public class Coordinates {
 	public final static Rectangle RECTANGLE_NOT_FOUND
-		= new Rectangle(10, 10, ComponentFigure.WIDTH, ComponentFigure.MIN_HEIGHT);
+		= new Rectangle(10, 10, BlockFigure.WIDTH, BlockFigure.MIN_HEIGHT);
 	
 	private WeakHashMap<DiagramType, Map<String, Rectangle>> coordinates;
 	

org.bloqqi.editor/src/org/bloqqi/editor/Palette.java
@@ -11,8 +11,8 @@ import org.eclipse.gef.requests.CreationFactory;
 
 import java.util.function.Supplier;
 
+import org.bloqqi.compiler.ast.Block;
 import org.bloqqi.compiler.ast.CompilationUnit;
-import org.bloqqi.compiler.ast.Component;
 import org.bloqqi.compiler.ast.Connection;
 import org.bloqqi.compiler.ast.InParameter;
 import org.bloqqi.compiler.ast.Modifiers;
@@ -21,7 +21,7 @@ import org.bloqqi.compiler.ast.StateVariable;
 import org.bloqqi.compiler.ast.TypeDecl;
 import org.bloqqi.compiler.ast.TypeUse;
 import org.bloqqi.compiler.ast.Variable;
-import org.bloqqi.editor.tools.ComponentCreationTool;
+import org.bloqqi.editor.tools.BlockCreationTool;
 import org.bloqqi.editor.tools.ConnectionCreationToolFeedback;
 import org.bloqqi.editor.tools.LiteralCreationTool;
 import org.bloqqi.editor.tools.ParameterCreationTool;
@@ -130,12 +130,12 @@ public class Palette extends PaletteRoot {
 
 	private ToolEntry createToolEntry(final TypeDecl td) {
 		CreationFactory factory = createFactory(
-				() -> new Component(new Modifiers(), new TypeUse(td.name()), null),
-				Component.class);
+				() -> new Block(new Modifiers(), new TypeUse(td.name()), null),
+				Block.class);
 		CreationToolEntry toolEntry = new CreationToolEntry(
-				td.name(), "Creates a new component of type " + td.name(),
+				td.name(), "Creates a new block of type " + td.name(),
 				factory, null, null);
-		toolEntry.setToolClass(ComponentCreationTool.class);
+		toolEntry.setToolClass(BlockCreationTool.class);
 		setToolProperties(toolEntry);
 		return toolEntry;
 	}

org.bloqqi.editor/src/org/bloqqi/editor/Properties.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Properties {
-	public static String KEY_COMPONENT_NAME;
+	public static String KEY_BLOCK_NAME;
 	
 	private Map<String, Object> map;
 	private Properties() {

org.bloqqi.editor/src/org/bloqqi/editor/Utils.java
@@ -14,8 +14,8 @@ import org.eclipse.ui.IFileEditorInput;
 import org.bloqqi.compiler.ast.ASTNode;
 import org.bloqqi.compiler.ast.Annotation;
 import org.bloqqi.compiler.ast.AutoLayoutConfig;
+import org.bloqqi.compiler.ast.Block;
 import org.bloqqi.compiler.ast.CompilationUnit;
-import org.bloqqi.compiler.ast.Component;
 import org.bloqqi.compiler.ast.DiagramType;
 import org.bloqqi.compiler.ast.Node;
 import org.bloqqi.compiler.ast.BloqqiParser;
@@ -139,12 +139,12 @@ public final class Utils {
 		
 		Map<String, Rectangle> map = new HashMap<String, Rectangle>();
 		int maxX = 0;
-		for (Component comp: dt.getComponents()) {
-			int x = comp.autoLayoutX(c);
-			int y = comp.autoLayoutY(c);
+		for (Block block: dt.getBlocks()) {
+			int x = block.autoLayoutX(c);
+			int y = block.autoLayoutY(c);
 			if (maxX < x) maxX = x;
 			Rectangle r = new Rectangle(x, y, c.width, c.height);
-			map.put(comp.accessString(), r);
+			map.put(block.accessString(), r);
 		}
 		
 		for (int i = 0; i < dt.getNumInParameter(); i++) {

org.bloqqi.editor/src/org/bloqqi/editor/actions/ChangeAllConnectionRoutersAction.java
@@ -30,8 +30,8 @@ public class ChangeAllConnectionRoutersAction extends MyWorkbenchPartAction {
 		DiagramTypePart dtp = (DiagramTypePart) root.getContents();
 		
 		for (Object child: dtp.getChildren()) {
-			EditPart component = (EditPart) child;
-			for (Object child2: component.getChildren()) {
+			EditPart block = (EditPart) child;
+			for (Object child2: block.getChildren()) {
 				AbstractGraphicalEditPart port = (AbstractGraphicalEditPart) child2;
 				for (Object conn: port.getSourceConnections()) {
 					ConnectionPart connectionPart = (ConnectionPart) conn;

commit 2ec292a88a37cb7c7e1fc644799924f10561e430

Fix inline bug introduced in 10be9a6

org.bloqqi.editor/src/org/bloqqi/editor/commands/SetBlockInlineCommand.java
@@ -76,15 +76,15 @@ public class SetBlockInlineCommand extends Command {
 		}
 
 		// Update coordinates of the inlined blocks
-		for (InheritedBlock block: diagramType.getBlocks()) {
-			if (block.isInlined()
-					&& block.inlinedBlock() == block
-					&& coordinates.getRectangle(diagramType, block.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
-				Rectangle r = adjustedLayout.get(block.accessString());
+		for (InheritedBlock b: diagramType.getBlocks()) {
+			if (b.isInlined()
+					&& b.inlinedBlock() == block
+					&& coordinates.getRectangle(diagramType, b.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
+				Rectangle r = adjustedLayout.get(b.accessString());
 				if (r == null) r = Coordinates.RECTANGLE_NOT_FOUND;
 				coordinates.setRectangle(
 						diagramType,
-						block.accessString(),
+						b.accessString(),
 						r);
 			}
 		}
@@ -93,9 +93,9 @@ public class SetBlockInlineCommand extends Command {
 	private void autoLayoutCollapsedBlock() {
 		if (coordinates.getRectangle(diagramType, block.accessString()) == Coordinates.RECTANGLE_NOT_FOUND) {
 			Rectangle closestToOrigin = null;
-			for (InheritedBlock block: diagramType.getBlocks()) {
-				if (block.isInlined() && block.inlinedBlock() == block) {
-					Rectangle r = coordinates.getRectangle(diagramType, block.accessString());
+			for (InheritedBlock b: diagramType.getBlocks()) {
+				if (b.isInlined() && b.inlinedBlock() == block) {
+					Rectangle r = coordinates.getRectangle(diagramType, b.accessString());
 					if (closestToOrigin == null || distance(r) < distance(closestToOrigin)) {
 						closestToOrigin = r;
 					}