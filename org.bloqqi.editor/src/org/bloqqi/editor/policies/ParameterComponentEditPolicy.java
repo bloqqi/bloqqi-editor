bloqqi/bloqqi-editor

commit 5a7500ff0b685f3cff734cb2e2fad32042447fef

Parameters can be deleted in diagram view
Fix #1

org.bloqqi.editor/src/org/bloqqi/editor/policies/ParameterComponentEditPolicy.java
@@ -1,0 +1,39 @@
+package org.bloqqi.editor.policies;
+
+import java.util.List;
+
+import org.bloqqi.compiler.ast.DiagramType;
+import org.bloqqi.compiler.ast.InParameter;
+import org.bloqqi.compiler.ast.OutParameter;
+import org.bloqqi.compiler.ast.Parameter;
+import org.bloqqi.editor.commands.ChangeSuperTypesAndParametersCommand;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.editpolicies.ComponentEditPolicy;
+import org.eclipse.gef.requests.GroupRequest;
+
+public class ParameterComponentEditPolicy extends ComponentEditPolicy {
+	@Override
+	protected Command createDeleteCommand(GroupRequest request) {
+		DiagramType dt = (DiagramType) getHost().getParent().getModel();
+		Parameter par = (Parameter) getHost().getModel();
+		
+		if (!par.isInherited()) {
+			ChangeSuperTypesAndParametersCommand cmd = new ChangeSuperTypesAndParametersCommand(dt);
+			cmd.setNewSuperTypesAsBefore();
+			if (par.isInParameter()) {
+				List<InParameter> inPars = dt.getLocalInParameters().asJavaUtilList();
+				inPars.remove(par.declaredParameter());
+				cmd.setNewInParameters(inPars);
+				cmd.setNewOutParametersAsBefore();
+			} else {
+				List<OutParameter> outPars = dt.getLocalOutParameters().asJavaUtilList();
+				outPars.remove(par.declaredParameter());
+				cmd.setNewOutParameters(outPars);
+				cmd.setNewInParametersAsBefore();
+			}
+			return cmd;
+		} else {
+			return null;
+		}
+	}
+}

org.bloqqi.editor/src/org/bloqqi/editor/commands/ChangeSuperTypesAndParametersCommand.java
@@ -63,6 +63,9 @@ public class ChangeSuperTypesAndParametersCommand extends Command {
 		removedSuperTypes.removeAll(allNewSuperTypes);
 		removedSuperTypes.remove(diagramType);
 	}
+	public void setNewSuperTypesAsBefore() {
+		newSuperTypes = diagramType.getSuperTypes().treeCopy();
+	}
 	
 	public void setNewInParameters(java.util.List<InParameter> inParameters) {
 		newInParameters = new List<>();
@@ -81,6 +84,9 @@ public class ChangeSuperTypesAndParametersCommand extends Command {
 				removedInParameters.add(in);
 			}
 		}
+	}
+	public void setNewInParametersAsBefore() {
+		newInParameters = diagramType.getLocalInParameters().treeCopy();
 	}
 	
 	public void setNewOutParameters(java.util.List<OutParameter> outParameters) {
@@ -101,6 +107,9 @@ public class ChangeSuperTypesAndParametersCommand extends Command {
 			}
 		}
 	}
+	public void setNewOutParametersAsBefore() {
+		newOutParameters = diagramType.getLocalOutParameters().treeCopy();
+	}
 
 	@Override
 	public boolean canExecute() {
@@ -120,9 +129,11 @@ public class ChangeSuperTypesAndParametersCommand extends Command {
 	private void removeAffectedConnections() {
 		// Step 1: identify connections to remove
 		Set<FlowDecl> removeFlowDecls = new HashSet<>();
-		for (FlowDecl fd: diagramType.getFlowDecls()) {
-			if (connectedToRemovedSuperType(fd)) {
-				removeFlowDecls.add(fd);
+		if (!removedSuperTypes.isEmpty()) {
+			for (FlowDecl fd: diagramType.getFlowDecls()) {
+				if (connectedToRemovedSuperType(fd)) {
+					removeFlowDecls.add(fd);
+				}
 			}
 		}
 		for (InheritedInParameter in: removedInParameters) {

org.bloqqi.editor/src/org/bloqqi/editor/editparts/ParameterPart.java
@@ -14,6 +14,7 @@ import org.bloqqi.compiler.ast.Parameter;
 import org.bloqqi.editor.Properties;
 import org.bloqqi.editor.figures.ParameterFigure;
 import org.bloqqi.editor.policies.NodeGraphicalNodeEditPolicy;
+import org.bloqqi.editor.policies.ParameterComponentEditPolicy;
 
 abstract public class ParameterPart<T extends Parameter> extends AbstractNodePart<T>
 		implements ASTObserver, NodeEditPart {
@@ -87,6 +88,7 @@ abstract public class ParameterPart<T extends Parameter> extends AbstractNodePar
 	@Override
 	protected void createEditPolicies() {
 		installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, new NodeGraphicalNodeEditPolicy());
+		installEditPolicy(EditPolicy.COMPONENT_ROLE, new ParameterComponentEditPolicy());
 	}
 	
 	@Override

commit ed7d8a87984ae8dd951995af4c93f256694c9da9

Fix bug when selecting parameters (introduced in 5a7500f)

org.bloqqi.editor/src/org/bloqqi/editor/policies/ParameterComponentEditPolicy.java
@@ -18,15 +18,18 @@ public class ParameterComponentEditPolicy extends ComponentEditPolicy {
 		Parameter par = (Parameter) getHost().getModel();
 		
 		if (!par.isInherited()) {
+			// This command is created when a parameter is selected.
+			// It would be possible to optimize the code by delaying the copying
+			// of AST structures and only copy when the command is executed.
 			ChangeSuperTypesAndParametersCommand cmd = new ChangeSuperTypesAndParametersCommand(dt);
 			cmd.setNewSuperTypesAsBefore();
 			if (par.isInParameter()) {
-				List<InParameter> inPars = dt.getLocalInParameters().asJavaUtilList();
+				List<InParameter> inPars = dt.getLocalInParameters().treeCopy().asJavaUtilList();
 				inPars.remove(par.declaredParameter());
 				cmd.setNewInParameters(inPars);
 				cmd.setNewOutParametersAsBefore();
 			} else {
-				List<OutParameter> outPars = dt.getLocalOutParameters().asJavaUtilList();
+				List<OutParameter> outPars = dt.getLocalOutParameters().treeCopy().asJavaUtilList();
 				outPars.remove(par.declaredParameter());
 				cmd.setNewOutParameters(outPars);
 				cmd.setNewInParametersAsBefore();